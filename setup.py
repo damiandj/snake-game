b8209bec7b735afe773af363d34268992d94a0c6
damjab <damianjablczyk@gmail.com>
2024-03-24 12:46:14 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as fp:
+    install_requires = fp.read()
+
+setup(
+    name="snake_game",
+    # version=tag,
+    description="Tool for manual image labeling",
+    url="https://github.com/damiandj/snake-game/",
+    author="Damian Jabłczyk",
+    author_email="damianjablczyk@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=find_packages(),
+    install_requires=install_requires,
+    dependency_links=[],
+    extras_require={},
+    package_data={},
+    data_files=[],
+    entry_points={
+        "console_scripts": [
+            "snake=snake_game.run_game_with_gui:main",
+        ],
+    },
+)
